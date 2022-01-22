cda5a5b15e03332cb690e8c4d1adb61288e8a151
Rajan Agarwal <64426829+rajanwastaken@users.noreply.github.com>
2022-01-22 05:09:33 +0000

    push new minimal concept

---
+++ components/Transition.tsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import ReactTextTransition, { presets } from "react-text-transition";
+
+const texts = [
+  "rajan.",
+  "a designer.",
+  "a developer.",
+	"an author.",
+	"a hacker.",
+	"curious.",
+	"a writer.",
+];
+
+class Transition extends Component {
+  state = {
+    textIndex: 1,
+    textFastIndex: 1,
+    paragraphIndex: 1,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        textIndex: this.state.textIndex + 13,
+        paragraphIndex: this.state.paragraphIndex + 1,
+      });
+    }, 3000);
+    setInterval(() => {
+      this.setState({
+        textFastIndex: this.state.textFastIndex + 1,
+      });
+    }, 150);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <section>
+          <section className="inline">
+					i&#39;m
+            <ReactTextTransition
+              className="transition"
+              text={texts[this.state.textIndex % texts.length]}
+              springConfig={presets.gentle}
+              style={{ margin: "0 5px" }}
+              inline
+            />
+          </section>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Transition;
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { Tooltip } from '@geist-ui/react';
 import Image from 'next/image';
-import Footer from '../components/Footer';
+import Transition from '../components/Transition';
 import { Container, Col, Row } from 'react-grid-system';
 import FadeIn from 'react-fade-in';
 
@@ -27,70 +27,60 @@ const Index = () => {
   <meta name="author" content="Rajan Agarwal"></meta>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 		</Head>
-		
+
     <Flex 
     px={4} 
-    pt={{ base: '12', md: '24' }} 
-    pb={8} 
     mx="auto"
+		py="37vh"
     bg="#090c0d"
     color="white">
-      <Box w="full" align="left" justify="center" mx="auto" px={{ base: '2%', md: '20%' }}>
+      <Box w="full" align="left" justify="center" mx="auto" px={{ base: '0%', md: '17%' }}>
        
 <Row>
+
+
 <Col sm={12}>
-<FadeIn delay={1000} transitionDuration={1000}>
+<FadeIn delay={1300} transitionDuration={800}>
  <Heading
           as="h2"
           fontFamily="Newsreader"
           my={3}
 					pb="1"
-					pt="2"
-          fontSize={{ base: '1.4em', md: "1.5em" }}
+					align="center"
+          fontSize={{ base: '1.3em', md: "1.5em" }}
           fontWeight="500"
           color={useColorModeValue("gray.100", "white")}
         >
-          it all began with a few robots, and then a few websites. 
+          inspired by methodical creation, i work on high-impact projects.
         </Heading>
 
 				 <Heading
           as="h2"
 					my={3}
 					pb="1"
+					align="center"
 					pt="2"
           fontFamily="Newsreader"
-          fontSize={{ base: '1.4em', md: "1.5em" }}
+          fontSize={{ base: '1.3em', md: "1.5em" }}
           fontWeight="500"
           color={useColorModeValue("gray.100", "white")}
         >
-          inspired by methodical creation, i began my work on high-impact projects.
-        </Heading>
-
-				<Heading
-          as="h2"
-					my={3}
-					pb="1"
-					pt="2"
-          fontFamily="Newsreader"
-          fontSize={{ base: '1.4em', md: "1.5em" }}
-          fontWeight="500"
-          color={useColorModeValue("gray.100", "white")}
-        >
-         currently, i&#39;m researching custom blockchains and actionable biotech.
+          within the community, i initiate actionable ventures.
         </Heading>
 
 				<Heading
 					my={3}
 					pb="1"
+					align="center"
 					pt="2"
           fontFamily="Newsreader"
-          fontSize={{ base: '1.4em', md: "1.5em" }}
+          fontSize={{ base: '1.3em', md: "1.5em" }}
           fontWeight="500"
           color={useColorModeValue("gray.100", "white")}
         >
-         i&#39;m rajan: hacker, author and developer.
-        </Heading>
+         <Transition/>
 
+        </Heading>
   </FadeIn>
 </Col>
 		</Row>
@@ -98,8 +88,12 @@ const Index = () => {
 
 
       </Box>
+
+			
 	
     </Flex>
+
+
 			</Container>
   );
 };
